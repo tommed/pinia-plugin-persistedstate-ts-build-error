07a2c5bdc84ac51a3c2a997dd3f696b8e6adbd69
Tom Medhurst <tom.medhurst@bradyplc.com>
2025-12-29 17:58:39 +0000

    Initial commit

---
+++ example1/src/stores/counter.ts
@@ -0,0 +1,16 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useCounterStore = defineStore('counter', () => {
+  const count = ref<number>(0)
+  const doubleCount = computed<number>(() => count.value * 2)
+  function increment() {
+    count.value++
+  }
+
+  return { count, doubleCount, increment }
+}, {
+  persist: {
+    storage: sessionStorage
+  }
+})
